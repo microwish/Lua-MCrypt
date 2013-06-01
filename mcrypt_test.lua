0588d4889c2dc3bf9d140a5cd204cc3ff08fa78e
microwish <microwish@gmail.com>
2013-06-02 01:41:20 +0800

    Update mcrypt_test.lua

--- mcrypt_test.lua
+++ mcrypt_test.lua
@@ -1,4 +1,4 @@
-package.cpath = package.cpath .. ";/home/microwish/lua-mcrypt/lib/?.so"
+package.cpath = package.cpath .. ";/home/microwish/lua-mcrypt-baidu/lib/?.so"
 
 local mcrypt = require("mcrypt")
 
@@ -9,10 +9,13 @@ mcrypt.module_open("rijndael-128", "", "cfb", "")
 local key_size, iv_size = mcrypt.enc_get_key_size(), mcrypt.enc_get_iv_size()
 print("this key size: " .. key_size, "this iv size: " .. iv_size)
 
-mcrypt.generic_init("12345678poiuytre", "0987qwer")
+mcrypt.generic_init("12345678poiuytre", "0987qwer0987qwer")
+--mcrypt.generic_init("12345678poiuytre", "0987qwer")
 
---local ciphered = mcrypt.generic("abcde")
-local ciphered = mcrypt.de_generic(string.char(211, 113, 129, 171, 126))
+local ciphered = mcrypt.generic("abcde")
+--local ciphered = mcrypt.de_generic(string.char(93, 135, 253, 58, 142))
+--local ciphered = mcrypt.de_generic(string.char(211, 113, 129, 171, 126))
+
 print(string.byte(ciphered, 1, 5))
 
 mcrypt.generic_deinit()
